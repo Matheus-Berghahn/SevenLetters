ec6622506bc8431bd558ac7b363ec8ddf3008feb
Gregore Gomes <gregore994@gmail.com>
2023-01-23 23:00:45 -0300

    initial project

---
+++ page/src/App.js
@@ -0,0 +1,15 @@
+import './App.scss';
+import Header from './Header';
+import Footer from './Header';
+
+function App() {
+  return (
+  <>
+    <Header />
+      <div><p>teste</p> </div>
+    <Footer />
+  </> 
+  );
+}
+
+export default App;
