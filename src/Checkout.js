352ef4e21caa422770d271da07186e0e3a541c27
Gregore Gomes <gregore994@gmail.com>
2023-01-25 19:29:22 -0300

    tela checkout production

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,10 +1,12 @@
-//import React, { useState } from 'react';
+import React, { useState } from 'react';
 import './Checkout.scss';
 
 const produtos = {
   nome: "xicara",
-  preco: 10500
+  preco: 10500,
+  estoque: 10
 }
+
 
 const Checkout = () => {
 
@@ -27,33 +29,77 @@ const Checkout = () => {
     // mandar para api de pagamento
   };
 
-    return (
-      <div className='checkout'>
-        <form onSubmit={handleSubmit}>
-          <div className='info-left'>
-            <h2>Pagamento</h2>
-            <div className='optionCheckout'>
-              <input type="checkbox"></input>
-              <h3>Cartão de Crédito</h3>
-              <div>
-              </div>
-            </div>
-            <div className='optionCheckout'>
-              <input type="checkbox"></input>
-              <h3>Boleto</h3>
-            </div>
-            <div className='optionCheckout'>
-              <input type="checkbox"></input>
-              <h3>PIX</h3>
-            </div>
+  const [selected, setSelected] = useState('yes');
+  
+  const handleChange = event => {
+    setSelected(event.target.value);
+    console.log(event.target.value);
+  };
+
+  return (
+
+    <div className='checkout'>
+      <form onSubmit={handleSubmit}>
+        <div className='info-left'>
+          <h2>Pagamento</h2>
+
+          <div className='optionCheckout'>
+            <input type="radio"
+            className='radio'
+            id="card"
+            name="choose"
+            value="card"
+            checked={selected === 'card'}
+            onChange={handleChange} >
+            </input>
+            <label htmlFor='card'>Cartão de Crédito</label>
           </div>
-        </form>
-          <div className='info-rigth'>
-            <h2>Resumo</h2>
+          <div className='optionCheckout'>
+            <input  type="radio"
+            className='radio'
+            id="boleto"
+            name="choose"
+            value="boleto"
+            onChange={handleChange}
+            checked={selected === 'boleto'}>
+          </input>
+            <label htmlFor='boleto'>Boleto</label>
+          </div>
+          <div className='optionCheckout'>
+            <input type="radio"
+            className='radio'
+            id="pix"
+            name="choose"
+            value="pix"
+            onChange={handleChange}
+            checked={selected === 'pix'}>
+          </input>
+            <label htmlFor='pix'>PIX</label>
+          </div>
+        </div>
+      </form>
+        <div className='info-rigth'>
+          <h2>Resumo</h2>
+          <span className='saparation'></span>
+          <div className='contentBox'>
             <p>{produtos.nome}</p>
+            <div className='labelQuant'>
+              <label htmlFor='quant'>Quantidade</label>
+              <input type="number" id="quant" name="quant" min="0" max={produtos.estoque} required />
+            </div>
             <p>{produtos.preco}</p>
           </div>
-      </div>
+          <span className='saparation'></span>
+          <div className='contentBox'>
+            <p>Subtotal<span>{`R$${produtos.preco}`}</span></p>
+            <p>Frete<span>{`R$${produtos.preco}`}</span></p>
+          </div>
+          <span className='saparation'></span>
+          <div className='contentBox'>
+            <b><p>Total<span>{`R$${produtos.preco}`}</span></p></b>
+          </div>
+        </div>
+    </div>
     );
   };
 
