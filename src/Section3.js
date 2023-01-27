43228386b191aad1821361472fce36f8aec18c49
matheus berghahn <matheus.berghahn@digitalbusiness.com.br>
2023-01-27 00:23:50 -0300

    ajuste

--- src/Section3.js
+++ src/Section3.js
@@ -7,9 +7,16 @@ function Section3() {
 
   return (
     <div className="section3">
-        <Parallax bgImage={ shape2 } strength={500}>
-        <div className='contentOne_middle'>
-          <a href='/checkout' className='contentOne_btn contentOne_btn1'>Saiba Mais</a>
+      <Parallax bgImage={ shape2 } strength={500}>
+        <div className="txt_parallax">
+          <h2>Lorem ipsum dolor sit amet</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse non accumsan urna. 
+          In at enim lectus. Nunc consectetur ut urna vel mattis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+          Suspendisse non accumsan urna. In at enim lectus. Nunc consectetur ut urna vel mattis.
+          </p>
+        </div>
+        <div className='contentTree_middle'>
+          <a href='/checkout' className='contentTree_btn contentTree_btn1'>Saiba Mais</a>
         </div>
       </Parallax>
       <div style={{ height: '100vh' }}></div>
