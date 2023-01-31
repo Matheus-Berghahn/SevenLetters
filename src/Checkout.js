436f04617d204dcf65b97e009a1f432020e61d56
Gregore Gomes <gregore994@gmail.com>
2023-01-31 12:35:51 -0300

    forms

--- src/Checkout.js
+++ src/Checkout.js
@@ -15,50 +15,68 @@ const produtos = [
 function Checkout() {
   const [firstForm, setFirstForm] = useState(true);
   const [secondForm, setSecondForm] = useState(false);
+  const [thirdForm, setThirdForm] = useState(false);
+  
   const [data, setData] = useState({});
-  const [isDisabled, setIsDisabled] = useState(true);
+  //const [isDisabled, setIsDisabled] = useState(true);
+
+  //primeiro form
 
   const handleFirstForm = e => {
     e.preventDefault();
     setData({ ...data, [e.target.name]: e.target.value });
-    setIsDisabled(!Object.values(data).every(Boolean));
+    //setIsDisabled(!Object.values(data).every(Boolean));
   };
-  console.log(data.CEP)
+
+  const handleClickFirstForm = e => {
+    setSecondForm(true)
+    setFirstForm(false)
+  };
+
+    //segundo form
 
   const handleSecondForm = e => {
     e.preventDefault();
     setData({ ...data, [e.target.name]: e.target.value });
   };
 
+  const handleClickSecondForm = e => {
+    setSecondForm(false)
+    setThirdForm(true)
+  };
+
+    //terceiro form
+
+  const handleThirdForm = e => {
+    e.preventDefault();
+    setData({...data, [e.target.name]: e.target.value});
+    console.log(secondForm)
+  };
+
   
 const options = [];
 produtos.forEach(produto => {
   for (let i = 0; i <= produto.quantidade; i++) {
-    options.push(<option value={i}>{i}</option>);
+    options.push(<option key={i} value={i}>{i}</option>);
   }
 });
 
-const [apiData, setApi] = useState([]);
+//const [apiData, setApi] = useState([]);
 
-useEffect(() => {
-  let cep = 93602210
-  const fetchData = async () => {
-    try {
-      const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
-      const apiData = await response.json();
-      setApi(apiData);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-  fetchData();
-}, []);
+// RODAR ESSA API NO SERVIDOR POR QUESTOES SE SEGURANCA
 
-console.log(apiData)
-
-
-
-
+// useEffect(() => {
+//   const fetchData = async () => {
+//     try {
+//       const response = await fetch(`https://viacep.com.br/ws/${data.CEP}/json/`);
+//       const apiData = await response.json();
+//       setApi(apiData);
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   };
+//   fetchData();
+// }, []);
 
 
   return (
@@ -71,101 +89,124 @@ console.log(apiData)
                 <h2>INFORMAÇÕES PESSOAIS</h2>
               </div>
               <div className='icons-group'>
-                <h2 className='icon'><FaUser /></h2>
-                <h2 className='icon'><MdLocationOn /></h2>
-                <h2 className='icon'><FaCreditCard /></h2>
-                <span></span>
+                <h2 className='icon user'><FaUser /></h2>
+                <h2 className='icon pin'><MdLocationOn /></h2>
+                <h2 className='icon card'><FaCreditCard /></h2>
+                {firstForm && (
+                  <span></span>
+                )}
+                {secondForm && (
+                  <span style={{width: `63%`, transition: `all 0.5s ease`}}></span>
+                )}
+                {thirdForm && (
+                  <span style={{width: `100%`}}></span>
+                )}
               </div>
           </div>
         
-        <div className='forms'>
-        {firstForm && (
-        <form onSubmit={e => setFirstForm(false)}>
-          <label htmlFor='nome'>NOME COMPLETO</label>
-          <input
-            type="text"
-            id='nome'
-            name="nome"
-            onChange={handleFirstForm}
-          />
-          <label htmlFor='email'>E-MAIL</label>
-          <input
-            id='email'
-            type="text"
-            name="e-mail"
-            onChange={handleFirstForm}
-          />
-          <label htmlFor='CPF'>CPF</label>
-          <input
-            id='CPF'
-            type="number"
-            name="CPF"
-            onChange={handleFirstForm}
-          />
-          <label htmlFor='telefone'>TELEFONE</label>
-          <input
-            id='telefone'
-            type="text"
-            name="telefone"
-            onChange={handleFirstForm}
-          />
-          <button disabled={isDisabled} type="submit">CONFIRMAR DADOS</button>
-        </form>
-      )}
-      {!firstForm && (
-        <form onSubmit={e => handleSecondForm(e)}>
-          <label htmlFor='cep'>CEP</label>
-          <input
-            id='cep'
-            type="text"
-            name="CEP"
-            placeholder="Ex. 12345000"
-            onChange={handleSecondForm}
-          />
-          <label htmlFor='estado'>Estado</label>
-          <input
-            id='estado'
-            type="text"
-            name="estado"
-            onChange={handleSecondForm}
-          />
-          <label htmlFor='cidade'>Cidade</label>
-          <input
-            id='cidade'
-            type="text"
-            name="cidade"
-            onChange={handleSecondForm}
-          />
-          <label htmlFor='bairro'>Bairro</label>
-          <input
-            id='bairro'
-            type="text"
-            name="bairro"
-            onChange={handleSecondForm}
-          />
-          <label htmlFor='numero'>Número da residência</label>
-          <input
-            id='numero'
-            type="text"
-            name="numero"
-            placeholder="Número ou s/n"
-            onChange={handleSecondForm}
-          />
-          <label htmlFor='complemento'>Complemento</label>
-          <input
-            id='complemento'
-            type="text"
-            name="numero"
-            placeholder="Apartamento, slaa, conjunto, edifício, andar, etc."
-            onChange={handleSecondForm}
-          />
-          <span>
-          <button type="submit">VOLTAR</button>
-          <button type="submit">CONFIRMAR DADOS</button>
-          </span>
-        </form>
-      )}
-        </div>
+          <div className='forms'>
+            {firstForm && (
+              <form>
+                <label htmlFor='nome'>NOME COMPLETO</label>
+                <input
+                  type="text"
+                  id='nome'
+                  name="nome"
+                  onChange={handleFirstForm}
+                />
+                <label htmlFor='email'>E-MAIL</label>
+                <input
+                  id='email'
+                  type="text"
+                  name="e-mail"
+                  onChange={handleFirstForm}
+                />
+                <label htmlFor='CPF'>CPF</label>
+                <input
+                  id='CPF'
+                  type="number"
+                  name="CPF"
+                  onChange={handleFirstForm}
+                />
+                <label htmlFor='telefone'>TELEFONE</label>
+                <input
+                  id='telefone'
+                  type="text"
+                  name="telefone"
+                  onChange={handleFirstForm}
+                />
+                <button onClick={handleClickFirstForm} disabled={false} type="submit">CONFIRMAR DADOS</button>
+              </form>
+            )}
+            {secondForm && (
+              <form>
+                <label htmlFor='cep'>CEP</label>
+                <input
+                  id='cep'
+                  type="text"
+                  name="CEP"
+                  placeholder="Ex. 12345000"
+                  onChange={handleSecondForm}
+                />
+                <label htmlFor='estado'>Estado</label>
+                <input
+                  id='estado'
+                  type="text"
+                  name="estado"
+                  onChange={handleSecondForm}
+                />
+                <label htmlFor='cidade'>Cidade</label>
+                <input
+                  id='cidade'
+                  type="text"
+                  name="cidade"
+                  onChange={handleSecondForm}
+                />
+                <label htmlFor='bairro'>Bairro</label>
+                <input
+                  id='bairro'
+                  type="text"
+                  name="bairro"
+                  onChange={handleSecondForm}
+                />
+                <label htmlFor='numero'>Número da residência</label>
+                <input
+                  id='numero'
+                  type="text"
+                  name="numero"
+                  placeholder="Número ou s/n"
+                  onChange={handleSecondForm}
+                />
+                <label htmlFor='complemento'>Complemento</label>
+                <input
+                  id='complemento'
+                  type="text"
+                  name="numero"
+                  placeholder="Apartamento, slaa, conjunto, edifício, andar, etc."
+                  onChange={handleSecondForm}
+                />
+                <span>
+                <button type="submit">VOLTAR</button>
+                <button onClick={handleClickSecondForm} type="submit">CONFIRMAR DADOS</button>
+                </span>
+              </form>
+            )}
+            {thirdForm && (
+              <form>
+                <label htmlFor='nome'>PAGAMENTO</label>
+                <input
+                  type="text"
+                  id='nome'
+                  name="nome"
+                  onChange={handleThirdForm}
+                />
+                <span>
+                <button type="submit">VOLTAR</button>
+                <button disabled={false} type="submit">CONFIRMAR DADOS</button>
+                </span>
+              </form>
+            )}
+          </div>
         </div>
         <div className='info-rigth'>
         <h2>DETALHES DA COMPRA</h2>
