176b89021c621f423e68c5e79673912b8c5ce6f1
Gregore Gomes <gregore994@gmail.com>
2023-03-24 13:26:57 -0300

    configurando cep

--- src/Checkout.js
+++ src/Checkout.js
@@ -2,10 +2,14 @@ import './Checkout.scss';
 import cardsAcepts from "./assets/images/cards.png";
 
 import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 //import { useParams } from "react-router-dom";
 import InputMask from "react-input-mask";
 import Cards from 'react-credit-cards-2';
 import 'react-credit-cards-2/es/styles-compiled.css';
+import { BsPinMap } from "react-icons/bs";
+import { MdPayment } from "react-icons/md";
+
 
 import { createTransaction } from "../src/services/api";
 
@@ -30,13 +34,28 @@ const Checkout = () => {
   const [loading, setLoading] = useState(false);
   const [paymentResponse, setPaymentResponse] = useState();
   
+  // BUSCA CEP
+
+  const {register, setValue} = useForm();
+
+  const handleBuscaCep = (e) => {
+    const cep = e.target.value.replace(/\D/g, '');
+    fetch(`https://viacep.com.br/ws/${cep}/json/`)
+    .then(res => res.json()).then(data => {
+      setValue("billingAddress", data.logradouro);
+      setValue("billingNeighborhood", data.bairro);
+      setValue("billingCity", data.localidade);
+      setValue("billingState", data.uf);
+    })
+  };
+
+   //
+  
   const handleSubmit = (e) => {
     e.preventDefault();
     
     (async () => {
       setLoading(true);
-      
-      console.log('teste')
       
       const response = await createTransaction({
         cartCode: 200,
@@ -60,12 +79,6 @@ const Checkout = () => {
       setPaymentResponse(response.data.status)
     })();
   };
-  
-  const [state, setState] = useState({
-    number: '',
-    expiry: '',
-    name: '',
-  });
 
   if (paymentResponse === "approved") {
     return(
@@ -86,165 +99,173 @@ const Checkout = () => {
           <form className="form" onSubmit={handleSubmit}>
             <div className='form-payment'>
               <div className='main-form'>
-                <div className='billing-form'>
-                <h3>Dados de entrega</h3>
-                  <div className='label-input'>
-                    <label htmlFor="name">Nome</label>
-                    <input
-                    type="text"
-                    name="name"
-                    id="name"
-                    value={customerName}
-                    onChange={(e) => setCustomerName(e.target.value)}
-                    />
-                  </div>
-
-                  <div className='label-input'>
-                    <label htmlFor="email">Email</label>
-                    <input
-                    type="email"
-                    name="email"
-                    id="email"
-                    value={customerEmail}
-                    onChange={(e) => setCustomerEmail(e.target.value)}
-                    />
-                  </div>
-                
-                  <div className='label-input'>
-                    <label htmlFor="document">CPF</label>
-                    <InputMask
-                    mask="999.999.999-99"
-                    type="text"
-                    name="document"
-                    id="document"
-                    value={customerDocument}
-                    onChange={(e) => setCustomerDocument(e.target.value)}
-                    />
-                  </div>
-                
-                  <div className='label-input'>
-                    <label htmlFor="mobile">Número de telefone</label>
-                    <InputMask
-                    mask="(99) 99999-9999"
-                    type="text"
-                    name="mobile"
-                    id="mobile"
-                    value={customerMobile}
-                    onChange={(e) => setCustomerMobile(e.target.value)}
-                    />
-                  </div>
-
-                  <div className='label-input'>
-                    <label htmlFor="zipcode">CEP</label>
-                    <InputMask
-                    mask="99999-999"
-                    type="text"
-                    name="zipcode"
-                    id="zipcode"
-                    value={billingZipCode}
-                    onChange={(e) => setBillingZipCode(e.target.value)}
-                    />
-                  </div>
-                  
-                  <div className='label-input'>
-                    <label htmlFor="address">Endereço</label>
-                    <input
-                    type="text"
-                    name="address"
-                    id="address"
-                    value={billingAddress}
-                    onChange={(e) => setBillingAddress(e.target.value)}
-                    />
-                  </div>
-                  <div className='englobe'>
-
+                  <div className='billing-form'>
+                    <div className='form-top'>
+                      <BsPinMap className='card-pin'/>
+                      <h3>Dados de entrega</h3>
+                    </div>
                     <div className='label-input'>
-                      <label htmlFor="number">Número</label>
+                      <label htmlFor="name">Nome</label>
                       <input
                       type="text"
-                      name="number"
-                      id="number"
-                      value={billingNumber}
-                      onChange={(e) => setBillingNumber(e.target.value)}
+                      name="name"
+                      id="name"
+                      value={customerName}
+                      onChange={(e) => setCustomerName(e.target.value)}
+                      />
+                    </div>
+
+                    <div className='label-input'>
+                      <label htmlFor="email">Email</label>
+                      <input
+                      type="email"
+                      name="email"
+                      id="email"
+                      value={customerEmail}
+                      onChange={(e) => setCustomerEmail(e.target.value)}
+                      />
+                    </div>
+                
+                    <div className='label-input'>
+                      <label htmlFor="document">CPF</label>
+                      <InputMask
+                      mask="999.999.999-99"
+                      type="text"
+                      name="document"
+                      id="document"
+                      value={customerDocument}
+                      onChange={(e) => setCustomerDocument(e.target.value)}
+                      />
+                    </div>
+                
+                    <div className='label-input'>
+                      <label htmlFor="mobile">Número de telefone</label>
+                      <InputMask
+                      mask="(99) 99999-9999"
+                      type="text"
+                      name="mobile"
+                      id="mobile"
+                      value={customerMobile}
+                      onChange={(e) => setCustomerMobile(e.target.value)}
+                      />
+                    </div>
+
+                    <div className='label-input'>
+                      <label htmlFor="zipcode">CEP</label>
+                      <InputMask
+                      {...register("billingZipCode")}
+                      mask="99999-999"
+                      type="text"
+                      name="zipcode"
+                      id="zipcode"
+                      value={billingZipCode}
+                      onChange={(e) => setBillingZipCode(e.target.value)}
+                      
+                      onBlur={handleBuscaCep}
                       />
                     </div>
                   
                     <div className='label-input'>
-                      <label htmlFor="neighborhood">Bairro</label>
+                      <label htmlFor="address">Endereço</label>
                       <input
+                      {...register("billingAddress")}
                       type="text"
-                      name="neighborhood"
-                      id="neighborhood"
-                      value={billingNeighborhood}
-                      onChange={(e) => setBillingNeighborhood(e.target.value)}
+                      name="address"
+                      id="address"
+                      value={billingAddress}
+                      onChange={(e) => setBillingAddress(e.target.value)}
+                      
                       />
                     </div>
+                    <div className='englobe'>
+
+                      <div className='label-input'>
+                        <label htmlFor="number">Número</label>
+                        <input
+                        type="text"
+                        name="number"
+                        id="number"
+                        value={billingNumber}
+                        onChange={(e) => setBillingNumber(e.target.value)}
+                        
+                        />
+                      </div>
+                    
+                      <div className='label-input'>
+                        <label htmlFor="neighborhood">Bairro</label>
+                        <input
+                        {...register("billingNeighborhood")}
+                        type="text"
+                        name="neighborhood"
+                        id="neighborhood"
+                        value={billingNeighborhood}
+                        onChange={(e) => setBillingNeighborhood(e.target.value)}
+                        
+                        />
+                      </div>
 
                   </div>
+
                   <div className='englobe'>
-                  <div className='label-input'>
-                  <label htmlFor="city">Cidade</label>
-                  <input
-                  type="text"
-                  name="city"
-                  id="city"
-                  value={billingCity}
-                  onChange={(e) => setBillingCity(e.target.value)}
-                  />
-                  </div>
+                    <div className='label-input'>
+                    <label htmlFor="city">Cidade</label>
+                    <input
+                    type="text"
+                    {...register("billingCity")}
+                    name="city"
+                    id="city"
+                    value={billingCity}
+                    onChange={(e) => setBillingCity(e.target.value)}
+                    
+                    />
+                    </div>
                 
                   <div className='label-input'>
-                  <label htmlFor="state">Estado</label>
-                  <select
-                  name="state"
-                  id="state"
-                  value={billingState}
-                  onChange={(e) => setBillingState(e.target.value)}>
-                  <option>Selecione</option>
-                  <option value="AC">Acre</option>
-                  <option value="AL">Alagoas</option>
-                  <option value="AP">Amapá</option>
-                  <option value="AM">Amazonas</option>
-                  <option value="BA">Bahia</option>
-                  <option value="CE">Ceará</option>
-                  <option value="DF">Distrito Federal</option>
-                  <option value="ES">Espírito Santo</option>
-                  <option value="GO">Goiás</option>
-                  <option value="MA">Maranhão</option>
-                  <option value="MT">Mato Grosso</option>
-                  <option value="MS">Mato Grosso do Sul</option>
-                  <option value="MG">Minas Gerais</option>
-                  <option value="PA">Pará</option>
-                  <option value="PB">Paraíba</option>
-                  <option value="PR">Paraná</option>
-                  <option value="PE">Pernambuco</option>
-                  <option value="PI">Piauí</option>
-                  <option value="RJ">Rio de Janeiro</option>
-                  <option value="RN">Rio Grande do Norte</option>
-                  <option value="RS">Rio Grande do Sul</option>
-                  <option value="RO">Rondônia</option>
-                  <option value="RR">Roraima</option>
-                  <option value="SC">Santa Catarina</option>
-                  <option value="SP">São Paulo</option>
-                  <option value="SE">Sergipe</option>
-                  <option value="TO">Tocantins</option>
-                  </select>
+                    <label htmlFor="state">Estado</label>
+                    <select
+                    {...register("billingState")}
+                    name="state"
+                    id="state"
+                    value={billingState}
+                    onChange={(e) => setBillingState(e.target.value)}>
+                      <option>Selecione</option>
+                      <option value="AC">Acre</option>
+                      <option value="AL">Alagoas</option>
+                      <option value="AP">Amapá</option>
+                      <option value="AM">Amazonas</option>
+                      <option value="BA">Bahia</option>
+                      <option value="CE">Ceará</option>
+                      <option value="DF">Distrito Federal</option>
+                      <option value="ES">Espírito Santo</option>
+                      <option value="GO">Goiás</option>
+                      <option value="MA">Maranhão</option>
+                      <option value="MT">Mato Grosso</option>
+                      <option value="MS">Mato Grosso do Sul</option>
+                      <option value="MG">Minas Gerais</option>
+                      <option value="PA">Pará</option>
+                      <option value="PB">Paraíba</option>
+                      <option value="PR">Paraná</option>
+                      <option value="PE">Pernambuco</option>
+                      <option value="PI">Piauí</option>
+                      <option value="RJ">Rio de Janeiro</option>
+                      <option value="RN">Rio Grande do Norte</option>
+                      <option value="RS">Rio Grande do Sul</option>
+                      <option value="RO">Rondônia</option>
+                      <option value="RR">Roraima</option>
+                      <option value="SC">Santa Catarina</option>
+                      <option value="SP">São Paulo</option>
+                      <option value="SE">Sergipe</option>
+                      <option value="TO">Tocantins</option>
+                    </select>
                   </div>
                 </div>
                   
                 </div>
                 <div className='payment-form'>
-                <div id='PaymentForm'>
-                
-                
-
-                </div>
-                  <h3>Dados de Pagamento</h3>
-                  <div className='cards-image'>
-                    <h4>Cartões aceitos:</h4>
-                      <img src={cardsAcepts} alt="cards"/>
+                  <div className='form-top'>
+                    <MdPayment className='card-icon'/>
+                    <h3>Dados de Pagamento</h3>
                   </div>
-                 
                   <div id='PaymentForm'>
                     <div className='cardState'>
                     <Cards
@@ -320,6 +341,10 @@ const Checkout = () => {
                   </div>
                   <div className='button'>
                     <button type="submit">CONFIRMAR COMPRA</button>
+                  </div>
+                  <div className='cards-image'>
+                    <h4>Cartões aceitos:</h4>
+                      <img src={cardsAcepts} alt="cards"/>
                   </div>
                 </div>
               </div>
